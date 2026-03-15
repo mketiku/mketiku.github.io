2c613e6a9395358dce7b56e68789d3e3c2fd0b99
michael ketiku <mketiku@gmail.com>
2026-03-15 10:10:13 -0400

    chore: migrate build system from static GitHub Pages to Astro

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+// TODO: consider migrating from Bootstrap to Tailwind CSS (run: npx astro add tailwind)
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://mketiku.github.io',
+  output: 'static',
+});
